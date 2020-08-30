cdd2273d9c774100fd2ab68f4ef409a80b647844
alex0blackwell <alex.blackwell314@gmail.com>
2020-08-30 13:55:57 -0700

    👌 IMPROVE: Add support for command line arguments

--- tree.py
+++ tree.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3
+
 from pathlib import Path
 from termcolor import cprint
 import os
+import sys
 
 class Tree():
     """docstring for Tree."""
 
-    def __init__(self):
-        self.currPath = Path(os.getcwd());
+    def __init__(self, showAll):
+        self.currPath = Path(os.getcwd())
+        self.showAll = showAll
         cprint((self.currPath.name), "blue")
 
 
@@ -14,21 +18,22 @@ class Tree():
         allFiles = []
         for file in self.currPath.iterdir():
             if(file.is_dir()):
-                spaces = ""
-                for i in range(indent-4):
-                    if(i % 4 == 0):
-                        spaces += '|'
-                    else:
-                        spaces += ' '
+                if( not(file.name[0] == '.' and not self.showAll)):
+                    spaces = ""
+                    for i in range(indent-4):
+                        if(i % 4 == 0):
+                            spaces += '|'
+                        else:
+                            spaces += ' '
 
-                formatIndent = spaces + '\\' + '-' * 4
+                    formatIndent = spaces + '\\' + '-' * 4
 
-                cprint(formatIndent, "white", end='')
-                cprint((file.name), "blue")
+                    cprint(formatIndent, "white", end='')
+                    cprint((file.name + '/'), "blue")
 
-                self.currPath = file;
-                self.print(indent + 4)
-            else:
+                    self.currPath = file;
+                    self.print(indent + 4)
+            elif( not(file.name[0] == '.' and not self.showAll)):
                 allFiles.append(file.name)
 
         for file in allFiles:
@@ -46,7 +51,24 @@ class Tree():
 
 
 def main():
-    tree = Tree()
+    showAll = False;
+    if(len(sys.argv) > 1):
+        if(sys.argv[1] == "-a" or sys.argv[1] == "--all"):
+            showAll = True
+        elif(sys.argv[1] == "-h" or sys.argv[1] == "--help"):
+            print('''tree - v-0.0.1 (2020 Aug. 30)\n
+Usage: tree [arguments] displays file structure.\n
+Arguments:\n
+-a  or  --all:    Include hidden files in tree visualization\n
+-h  or  --help:   Print Help (this message) and exit''')
+            sys.exit(1)
+        else:
+            print(f'''tree - v-0.0.1 (2020 Aug. 30)\n
+Garbage after option argument: "{sys.argv[1]}"\n
+More info with: "tree -h"''')
+            sys.exit(1)
+
+    tree = Tree(showAll)
     tree.print()
 
 
